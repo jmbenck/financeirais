4bab044d7881f883b154b7781e97bef649637731
jmbenck <jmbenck@gmail.com>
2020-09-22 21:26:54 -0300

    Change database password

--- financeirais/src/settings.py
+++ financeirais/src/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'financeirais',
         'USER':'root',
-        'PASSWORD':'',
+        'PASSWORD':'root',
         'HOST':'localhost',
         'PORT':'3306'
     }
